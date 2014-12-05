50fe37a290dda1d0d66ac1196b7aea0274d7d4d4
Calvert <yz3210@gmail.com>
2014-12-05 11:02:22 +0800

    Actually use the error reporting.
    chapter9-2

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -12,7 +12,7 @@ Template.postEdit.events({
     Posts.update(currentPostId, { $set: postProperties }, function(err) {
       if (err) {
         // display the error to the user
-        alert(error.reason);
+        throwError(error.reason);
       } else {
         Router.go('postPage', { _id: currentPostId });
       }
